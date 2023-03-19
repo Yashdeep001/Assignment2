3a0f9d3a904eeae8a18bb11f51f46baee80db07a
Yashdeep001 <112737611+Yashdeep001@users.noreply.github.com>
2023-03-19 15:13:04 -0600

    Add files via upload

---
+++ Data/Flight.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Data
+{
+
+	public class Flight
+	{
+	
+		public string FlightCode { get; set; }
+
+	
+		public string Origin { get; set; }
+
+	
+		public string Destination { get; set; }
+
+		public DayOfWeek DayOfWeek { get; set; }
+
+		
+		public string Airline { get; set; }
+
+	
+		public TimeSpan Time { get; set; }
+
+	
+		public int AvailableSeats { get; set; }
+
+	
+		public decimal Cost { get; set; }
+
+
+		public Flight(string flightCode, string airline, string origin, string destination, DayOfWeek dayOfWeek, TimeSpan time, int availableSeats, decimal cost)
+		{
+			FlightCode = flightCode;
+			Origin = origin;
+			Destination = destination;
+			DayOfWeek = dayOfWeek;
+			Airline = airline;
+			Time = time;
+			AvailableSeats = availableSeats;
+			Cost = cost;
+		}
+
+	
+		public override string ToString()
+		{
+			return $"{FlightCode} - {Airline}: {Origin} to {Destination} ({DayOfWeek} {Time:hh\\:mm}) - ${Cost:F2} - {AvailableSeats} seats available";
+		}
+	}
+}
---
+++ Data/Reservation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Data
+{
+
+	public class Reservation
+	{
+		
+		public string ReservationCode { get; set; }
+
+		
+		public Flight Flight { get; set; }
+
+		public string TravelerName { get; set; }
+
+		
+		public string Citizenship { get; set; }
+
+		
+		public bool IsActive { get; set; }
+
+	
+		public Reservation(string reservationCode, Flight flight, string name, string citizenship)
+		{
+			ReservationCode = reservationCode;
+			Flight = flight;
+			TravelerName = name;
+			Citizenship = citizenship;
+			IsActive = true;
+		}
+
+
+		public void Update(string name, string citizenship, bool isActive)
+		{
+			// if passenger name and citizenship are empty, throw an exception
+			if (string.IsNullOrEmpty(name))
+			{
+				throw new ArgumentException("Name cannot be empty.");
+			}
+
+			if (string.IsNullOrEmpty(citizenship))
+			{
+				throw new ArgumentException("Citizenship cannot be empty.");
+			}
+
+			TravelerName = name;
+			Citizenship = citizenship;
+			IsActive = isActive;
+		}
+
+	
+		public override string ToString()
+		{
+			string status = IsActive ? "Active" : "Inactive";
+			return $"Reservation Code: {ReservationCode}, Flight Code: {Flight.FlightCode}, Airline: {Flight.Airline}, " +
+				   $"Cost: {Flight.Cost:C}, Name: {TravelerName}, Citizenship: {Citizenship}, Status: {status}";
+		}
+	}
+}
